7edaec1a9085b8bcd956d3e41c8303e7747cd764
Lucas Renan <lucasrenandns96@gmail.com>
2022-05-28 12:33:58 -0300

    Update script.js

--- mini-projetos/calculadora-IMC/script.js
+++ mini-projetos/calculadora-IMC/script.js
@@ -15,25 +15,25 @@ function imc() {
 
     else {
         const valor_imc = (peso / (altura * altura)).toFixed(2)
-        let classificaçao = ''
+        let classificação = ''
 
         if (valor_imc < 18.5) {
-            classificaçao = ' abaixo do peso. Se cuida!'
+            classificação = ' abaixo do peso. Se cuida!'
         }
 
         else if (valor_imc >= 18.5 && valor_imc <= 24.9) {
-            classificaçao = ' no peso ideal. Parabéns!'
+            classificação = ' no peso ideal. Parabéns!'
         }
 
         else if (valor_imc >= 25 && valor_imc <= 29.9) {
-            classificaçao = ' com sobrepeso. Atenção!'
+            classificação = ' com sobrepeso. Atenção!'
         }
 
         else {
-            classificaçao = ' com obesidade. Cuidado!'
+            classificação = ' com obesidade. Cuidado!'
         }
 
-        resultado.textContent = `Olá ${nome.toUpperCase()}, seu IMC é ${valor_imc}. Você está ${classificaçao}!`
+        resultado.textContent = `Olá ${nome.toUpperCase()}, seu IMC é ${valor_imc}. Você está ${classificação}!`
     }
 
 }
